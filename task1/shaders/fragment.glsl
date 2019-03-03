5374989ed1eb4546a96993e5f0216ba2c92a08f9
daria25D <dasha_domracheva@mail.ru>
2019-03-03 14:49:20 +0300

    objects changed

--- task1/shaders/fragment.glsl
+++ task1/shaders/fragment.glsl
@@ -10,73 +10,42 @@ out vec4 color;
 
 const int NUM_OF_LIGHTS = 2;
 const float MAX_DIST = 100;
+
+struct Material {
+    vec3  color;
+    float shininess;
+    float k_diffuse;
+    float k_specular;
+    float k_ambient;
+    float k_occlusion;
+    float k_reflection;
+};
 //objects
 float sphere; //current distance
-vec3  sphere_centre     = vec3(0.0, 0.0, 0.0);
+vec3  sphere_centre     = vec3(-2.0, 0.0, 0.0);
 float sphere_radius     = 1.0;
-vec3  sphere_color      = vec3(0.9, 0.1, 0.1);
-float sphere_shininess  = 100.0;
-float sphere_diffuse    = 0.33;
-float sphere_specular   = 0.9;
-float sphere_ambient    = 0.33;
-float sphere_occlusion  = 5.0;
-float sphere_reflection = 0.0;
 
+float cone_for_sphere;
+vec3  cone_for_sphere_centre     = vec3(-2.0, -1.0, 0.0);
+vec2  cone_for_sphere_dimensions = vec2(0.7, 1.0);
 
 float ellipsoid;
-vec3  ellipsoid_centre     = vec3(2.0, -2.0, 0.0);
-vec3  ellipsoid_dimensions = vec3(1.0, 2.0, 0.5);
-vec3  ellipsoid_color      = vec3(0.0, 0.7, 0.0);
-float ellipsoid_shininess  = 2.0;
-float ellipsoid_diffuse    = 0.4;
-float ellipsoid_specular   = 0.2;
-float ellipsoid_ambient    = 0.4;
-float ellipsoid_occlusion  = 4.0;
-float ellipsoid_reflection = 0.0;
+vec3  ellipsoid_centre     = vec3(2.0, -0.95, 0.0);
+vec3  ellipsoid_dimensions = vec3(0.5, 0.3, 0.3);
 
 float torus;
-vec3  torus_centre     = vec3(-1.0, -5.0, 1.0);
-vec2  torus_dimensions = vec2(1.0, 0.5);
-vec3  torus_color      = vec3(0.2, 0.7, 0.7);
-float torus_shininess  = 50.0;
-float torus_diffuse    = 0.33;
-float torus_specular   = 0.5;
-float torus_ambient    = 0.33;
-float torus_occlusion  = 4.0;
-float torus_reflection = 0.0;
+vec3  torus_centre     = vec3(2.0, -2.5, 0.0);
+vec2  torus_dimensions = vec2(1.2, 0.15);
 
 float plane;
 vec3  plane_centre     = vec3(-1.0, -1.0, -1.0);
 vec4  plane_normal     = vec4(0.0, 1.0, 0.0, 5.0);
-vec3  plane_color      = vec3(0.8);
-float plane_shininess  = 2.0;
-float plane_diffuse    = 0.33;
-float plane_specular   = 0.5;
-float plane_ambient    = 0.33;
-float plane_occlusion  = 5.0;
-float plane_reflection = 1.0;
-
-float cube_for_sphere;
-vec3  cube_for_sphere_centre     = vec3(0.0, 0.0, 0.0);
-vec3  cube_for_sphere_dimensions = vec3(1.0);
 
 float cube;
-vec3  cube_centre     = vec3(2.0, -5.0, 0.0);
-vec3  cube_dimensions = vec3(1.0);
-vec3  cube_color      = vec3(0.5, 0.0, 0.5);
-float cube_shininess  = 3.0;
-float cube_diffuse    = 0.33;
-float cube_specular   = 0.5;
-float cube_ambient    = 0.33;
-float cube_occlusion  = 4.0;
-float cube_reflection = 0.0;
+vec3  cube_centre     = vec3(2.0, -4.5, 0.0);
+vec3  cube_dimensions = vec3(1.7, 1.5, 1.5);
 
-float shininess;
-float k_diffuse;
-float k_specular;
-float k_ambient;
-float k_occlusion;
-float k_reflection;
+Material material;
 
 //distance functions
 float distance_from_sphere(vec3 p, vec3 c, float r) {
@@ -101,9 +70,36 @@ float distance_from_cube(vec3 p, vec3 c, vec3 s) {
 
     return insideDistance + outsideDistance;
 }
+float distance_from_cone( vec3 p, vec3 c, vec2 r) {
+//    float q = length(p.xy - c.xy);
+//    return dot(normalize(r), vec2(q, p.z - c.z));
+    float height = r.x;
+    float radius = r.y;
+    vec2 q = vec2(length(p.xz - c.xz), p.y - c.y);
+    vec2 tip = q - vec2(0, height);
+    vec2 mantleDir = normalize(vec2(height, radius));
+    float mantle = dot(tip, mantleDir);
+    float d = max(mantle, -q.y);
+    float projected = dot(tip, vec2(mantleDir.y, -mantleDir.x));
+
+    // distance to tip
+    if ((q.y > height) && (projected < 0)) {
+        d = max(d, length(tip));
+    }
+
+    // distance to base ring
+    if ((q.x > radius) && (projected > length(vec2(height, radius)))) {
+        d = max(d, length(q - vec2(radius, 0)));
+    }
+    return d;
+}
 //changing the final shape
 float intersect(float dist_a, float dist_b) {
     return max(dist_a, dist_b);
+}
+float smooth_union(float d1, float d2, float k) {
+    float h = clamp( 0.5 + 0.5*(d2 - d1) / k, 0.0, 1.0 );
+    return mix(d2, d1, h) - k*h * (1.0 - h);
 }
 float twist_torus(vec3 p, vec3 c, vec2 t, float twists) {
     float k = cos(twists*(p.y - c.y));
@@ -123,14 +119,14 @@ float bend_torus(vec3 p, vec3 c, vec2 t, float bends) {
 float distance_to_surface(vec3 p, int num) {
     if (num == 0) {
         //return distance_from_sphere(p, vec3(0.0), 1.0);
-        return intersect(distance_from_sphere(p / 1.2, sphere_centre, sphere_radius) * 1.2,
-                         distance_from_cube(p, cube_for_sphere_centre, cube_for_sphere_dimensions));
+        return smooth_union(distance_from_sphere(p, sphere_centre, sphere_radius),
+                            distance_from_cone(p, cone_for_sphere_centre, cone_for_sphere_dimensions), 2);
     } else if (num == 1) {
         return distance_from_ellipsoid(p,  ellipsoid_centre, ellipsoid_dimensions);
     } else if (num == 2) {
-        return bend_torus(p, torus_centre, torus_dimensions, 0);
+        return twist_torus(p, torus_centre, torus_dimensions, 0);
     } else if (num == 3) {
-        return distance_from_plane(p, plane_centre, plane_normal);
+        return distance_from_plane(p, plane_centre, normalize(plane_normal));
     } else if (num == 4) {
         return distance_from_cube(p, cube_centre, cube_dimensions);
     }
@@ -157,59 +153,87 @@ vec3 calculate_normal(vec3 p) {
     return normalize(normal);
 }
 
+Material sphere_material() {
+    Material sphere_mat;
+    sphere_mat.color        = vec3(0.9, 0.1, 0.1);
+    sphere_mat.shininess    = 100.0;
+    sphere_mat.k_diffuse    = 0.33;
+    sphere_mat.k_specular   = 0.9;
+    sphere_mat.k_ambient    = 0.33;
+    sphere_mat.k_occlusion  = 5.0;
+    sphere_mat.k_reflection = 0.0;
+    return sphere_mat;
+}
+
+Material ellipsoid_material() {
+    Material ellipsoid_mat;
+    ellipsoid_mat.color        = vec3(0.9, 0.7, 0.8);
+    ellipsoid_mat.shininess    = 200.0;
+    ellipsoid_mat.k_diffuse    = 0.8;
+    ellipsoid_mat.k_specular   = 0.8;
+    ellipsoid_mat.k_ambient    = 0.6;
+    ellipsoid_mat.k_occlusion  = 4.0;
+    ellipsoid_mat.k_reflection = 0.0;
+    return ellipsoid_mat;
+}
+
+Material torus_material() {
+    Material torus_mat;
+    torus_mat.color        = vec3(0.9, 0.7, 0.0);
+    torus_mat.shininess    = 70.0;
+    torus_mat.k_diffuse    = 0.6;
+    torus_mat.k_specular   = 0.7;
+    torus_mat.k_ambient    = 0.5;
+    torus_mat.k_occlusion  = 4.0;
+    torus_mat.k_reflection = 0.0;
+    return torus_mat;
+}
+Material plane_material() {
+    Material plane_mat;
+    plane_mat.color        = vec3(0.8);
+    plane_mat.shininess    = 2.0;
+    plane_mat.k_diffuse    = 0.33;
+    plane_mat.k_specular   = 0.5;
+    plane_mat.k_ambient    = 0.33;
+    plane_mat.k_occlusion  = 5.0;
+    plane_mat.k_reflection = 1.0;
+    return plane_mat;
+}
+Material cube_material() {
+    Material cube_mat;
+    cube_mat.color        = vec3(0.5, 0.0, 0.5);
+    cube_mat.shininess    = 20.0;
+    cube_mat.k_diffuse    = 0.33;
+    cube_mat.k_specular   = 0.5;
+    cube_mat.k_ambient    = 0.33;
+    cube_mat.k_occlusion  = 4.0;
+    cube_mat.k_reflection = 0.0;
+    return cube_mat;
+}
+
 vec3 color_of_closest_object(vec3 p) {
     float d = 1e10;
     vec3 color = vec3(0.0);
     if (sphere < d) {
         d = sphere;
-        color = sphere_color;
-        shininess = sphere_shininess;
-        k_diffuse = sphere_diffuse;
-        k_specular = sphere_specular;
-        k_ambient = sphere_ambient;
-        k_occlusion = sphere_occlusion;
-        k_reflection = sphere_reflection;
+        material = sphere_material();
     }
     if (ellipsoid < d) {
         d = ellipsoid;
-        color = ellipsoid_color;
-        shininess = ellipsoid_shininess;
-        k_diffuse = ellipsoid_diffuse;
-        k_specular = ellipsoid_specular;
-        k_ambient = ellipsoid_ambient;
-        k_occlusion = ellipsoid_occlusion;
-        k_reflection = ellipsoid_reflection;
+        material = ellipsoid_material();
     }
     if (torus < d) {
         d = torus;
-        color = torus_color;
-        shininess = torus_shininess;
-        k_diffuse = torus_diffuse;
-        k_ambient = torus_ambient;
-        k_specular = torus_ambient;
-        k_occlusion = torus_occlusion;
-        k_reflection = torus_reflection;
+        material = torus_material();
     }
     if (plane < d) {
         d = plane;
-        color = plane_color;
-        shininess = plane_shininess;
-        k_diffuse = plane_diffuse;
-        k_ambient = plane_ambient;
-        k_specular = plane_ambient;
-        k_occlusion = plane_occlusion;
-        k_reflection = plane_reflection;
+        material = plane_material();
     } if (cube < d) {
         d = cube;
-        color = cube_color;
-        shininess = cube_shininess;
-        k_diffuse = cube_diffuse;
-        k_ambient = cube_ambient;
-        k_specular = cube_ambient;
-        k_occlusion = cube_occlusion;
-        k_reflection = cube_reflection;
+        material = cube_material();
     }
-    return color;
+    return material.color;
 }
 //to prevent self-illumination
 vec3 color_lerp(vec3 p) {
@@ -229,11 +253,11 @@ vec3 color_lerp(vec3 p) {
     if (plane > eps) plane_weight = GI/(plane + 1.0);
     if (cube > eps) cube_weight - GI/(cube + 1.0);
 
-    color = sphere_color * sphere_weight +
-            ellipsoid_color * ellipsoid_weight +
-            torus_color * torus_weight +
-            plane_color * plane_weight +
-            cube_color * cube_weight;
+    color = sphere_material().color * sphere_weight +
+            ellipsoid_material().color * ellipsoid_weight +
+            torus_material().color * torus_weight +
+            plane_material().color * plane_weight +
+            cube_material().color * cube_weight;
     return color * color * color * 0.15;
 }
 
@@ -281,7 +305,7 @@ vec3 render(vec3 ray_pos, vec3 ray_dir, vec3 light_position[NUM_OF_LIGHTS]) {
     vec3 normal = calculate_normal(pos);
     vec3 color = color_of_closest_object(pos);
     vec3 gi = color_lerp(pos);
-    if (t > MAX_DIST) {color = vec3(0.8, 0.8, 0.8); gi = vec3(0.0);}
+    if (t > MAX_DIST) {color = vec3(0.8); gi = vec3(0.0);}
     vec3 final_color = vec3(0.0);
     vec3 light_dir1 = normalize(light_position[0] - pos);
     vec3 light_dir2 = normalize(light_position[1] - pos);
@@ -290,29 +314,23 @@ vec3 render(vec3 ray_pos, vec3 ray_dir, vec3 light_position[NUM_OF_LIGHTS]) {
     vec3 reflection2 = normalize(light_dir2 + viewer_dir);
     float diffuse1 = clamp(dot(normal, light_dir1), 0.0, 1.0);
     float diffuse2 = clamp(dot(normal, light_dir2), 0.0, 1.0);
-    float specular1 = max(0.0, pow(dot(normal, reflection1), shininess)) * float(diffuse1 > 0.0);
-    float specular2 = max(0.0, pow(dot(normal, reflection2), shininess)) * float(diffuse2 > 0.0);
+    float specular1 = max(0.0, pow(dot(normal, reflection1), material.shininess)) * float(diffuse1 > 0.0);
+    float specular2 = max(0.0, pow(dot(normal, reflection2), material.shininess)) * float(diffuse2 > 0.0);
     //diffuse1 = 0.5 + 0.5 * diffuse1;
     //diffuse2 = 0.5 + 0.5 * diffuse2;
-    if (shininess > 100) {
-        specular1 = specular1/5;
-        specular2 = specular2/5;
-    }
-    float ambient_occ = ambient_occlusion(pos, normal, k_occlusion);
+    float ambient_occ = ambient_occlusion(pos, normal, material.k_occlusion);
     float shadow1 = soft_shadow(pos, normalize(light_position[0]));
     float shadow2 = soft_shadow(pos, normalize(light_position[1]));
     vec3 reflect = vec3(0.0);
-    final_color += vec3(((k_diffuse*(diffuse1 + diffuse2) + k_ambient*ambient_occ + k_reflection*reflect) * (color + gi))
-                        * 0.5*(shadow1 + shadow2) + k_specular*(specular1 + specular2)) ;
+    final_color += vec3(((material.k_diffuse*(diffuse1 + diffuse2) + material.k_ambient*ambient_occ + material.k_reflection*reflect) * (color + gi))
+                        * 0.5*(shadow1 + shadow2) + material.k_specular*(specular1 + specular2)) ;
     final_color *= exp(-0.00005*t*t);
     return final_color;
 }
-
-/**
+/*
 TODO:
-* try to think of how to add a second sourse of light
+* do materials structures instead of assigning everything to different values
 */
-
 void main() {
     vec2 tmp = 0.5 + vec2(fragmentTexCoord.x * g_screenWidth /2, fragmentTexCoord.y * g_screenHeight /2); //try to change
     vec3 ray_dir = normalize(vec3(tmp, -(g_screenWidth)/tan(3.14159265/3)));
@@ -321,8 +339,9 @@ void main() {
     vec3 ray_pos = vec3(0.0 + g_rayMatrix[3][0], 0.0 + g_rayMatrix[3][1], 5.0 + g_rayMatrix[3][2]); //+ rayMatrix[3][i]
 
     vec3 light_position[NUM_OF_LIGHTS];
-    light_position[0] = vec3(4.0, 7.0, 0.0);
-    light_position[1] = vec3(0.0, 7.0, 3.0);
+    light_position[0] = vec3(4.0, 7.0, 4.0);
+    light_position[1] = vec3(1.0, 7.0, -3.0);
+
 
     color = vec4(render(ray_pos, ray_dir, light_position), 1.0);
 }
